6e8eddd55891ee353fb4772e0c20e7c235586d66
Oskar <pawicao@gmail.com>
2020-01-15 13:21:05 +0100

    Capturing, game ending, piece counting

--- src/checkers.erl
+++ src/checkers.erl
@@ -1,11 +1,11 @@
 
 -module(checkers).
 -import(board,[showBoard/1,startingBoard/0,getSquareValue/2]).
--import(moves,[makeMove/3,getAvailableSquares/4]).
+-import(moves,[makeMove/4,getAvailableSquares/3,moveOrCapture/2]).
 %% API
--export([main/0, player/0]).
+-export([main/0, player/0,getOpponentColor/1]).
 
-getOppontentColor(C) ->
+getOpponentColor(C) ->
   case C of
     white -> black;
     black -> white
@@ -43,8 +43,7 @@ areValidPieces([X1,Y1,X2,Y2], Color, BoardState) ->
 
 	
 checkIfMoveIsLegal([X1,Y1,X2,Y2], BoardState, Color) ->
-	CaptureMoves = [], 
-	Z = getAvailableSquares(getSquareValue({X1,Y1},BoardState),{X1,Y1},BoardState, CaptureMoves),
+	Z = getAvailableSquares(getSquareValue({X1,Y1},BoardState),{X1,Y1},BoardState),
 	case lists:member({X2,Y2},Z) of
 		false ->
 			io:format("Ilegal move! ~n"),
@@ -77,7 +76,7 @@ getPlayerMoveIfSquaresAreValid(BoardState,Color) ->
     {true, Move} -> 
 	  case areValidPieces(Move, Color, BoardState) of
 		false ->
-			io:format("Wrong piece chosen! ~n"),
+			io:format("Ilegal move! ~n"),
 			getPlayerMoveIfSquaresAreValid(BoardState,Color);
 		true ->
 			checkIfMoveIsLegal(Move, BoardState, Color)
@@ -88,17 +87,23 @@ getPlayerMove() ->
   [IX1, IY1, IX2, IY2] = readPlayerMove(),
   [inputToColNo(IX1), inputToRowNo(IY1), inputToColNo(IX2), inputToRowNo(IY2)].
 
+checkForEndGame({WhiteCount,BlackCount},Color,PIDMain) ->
+	case {WhiteCount,BlackCount} of
+		{{Color,0},_} -> PIDMain!{end_of_game,getOpponentColor(Color)};
+		{_,{Color,0}} -> PIDMain!{end_of_game,getOpponentColor(Color)};
+		_ 			  -> true
+	end.
+  
 player() ->
   receive
-    {PIDMain,PIDOpponent,Color,BoardState} ->
+    {PIDMain,PIDOpponent,Color,BoardState,PieceCount} ->
+	  checkForEndGame(PieceCount,Color,PIDMain),
       PIDNewMe = spawn(?MODULE,player,[]),
       showBoard(BoardState),
       io:format("~s to move ~n",[Color]),
       [X1, Y1, X2, Y2] = getPlayerMoveIfSquaresAreValid(BoardState,Color),
-      NewBoardState = makeMove({X1,Y1},{X2,Y2},BoardState),
-      PIDOpponent!{PIDMain,PIDNewMe,getOppontentColor(Color),NewBoardState},
-      erlang:display("DEBUG")
-      %PIDMain!{end_of_game,getOppontentColor(Color)}
+      {NewPieceCount,NewBoardState} = makeMove([X1, Y1, X2, Y2],BoardState,Color,PieceCount),
+      PIDOpponent!{PIDMain,PIDNewMe,getOpponentColor(Color),NewBoardState,NewPieceCount}
   end.
 
 main() ->
@@ -112,12 +117,12 @@ main() ->
 	  %io:format("\e[H\e[J"),
       PIDPlayer1 = spawn(?MODULE,player,[]),
       PIDPlayer2 = spawn(?MODULE,player,[]),
-      PIDPlayer1!{self(),PIDPlayer2,white,startingBoard()},
+      PIDPlayer1!{self(),PIDPlayer2,white,startingBoard(),{{white,12},{black,12}}},
       receive
-        {end_of_game, Color} -> io:format("Thanks for playing. ~s won!", [Color])
+        {end_of_game, Color} -> io:format("Thanks for playing. ~s won!", [Color]),erlang:halt()
       end;
     2 ->
-      io:format("In version 2.0");
+      io:format("In version 2.0"),io:format("In version 2.0");
     3 ->
       io:format("Thanks for playing.");
     _ ->
--- src/moves.erl
+++ src/moves.erl
@@ -1,18 +1,36 @@
-
 -module(moves).
 -import(board,[getSquareValue/2,setSquareValue/3]).
+-import(checkers,[getOpponentColor/1]).
 %% API
--export([makeMove/3, getNormalMoveSquares/4,getAvailableSquares/4]).
+-export([makeMove/4, getNormalMoveSquares/3,getAvailableSquares/3,moveOrCapture/2]).
 
-makeMove({X1,Y1},{X2,Y2},BoardState) ->
+makeMove([X1, Y1, X2, Y2],BoardState,Color,PieceCount) ->
   BoardStateTmp = setSquareValue(empty, {X1,Y1}, BoardState),
-  setSquareValue(getSquareValue({X1,Y1}, BoardState), {X2,Y2}, BoardStateTmp).
+  BoardStateTmp2 = moveOrCapture([X1, Y1, X2, Y2],BoardStateTmp),
+  case BoardStateTmp2 of
+	BoardStateTmp -> {PieceCount,setSquareValue(getSquareValue({X1,Y1}, BoardState), {X2,Y2}, BoardStateTmp2)};
+	_			  -> {decrementPiece(getOpponentColor(Color),PieceCount),setSquareValue(getSquareValue({X1,Y1}, BoardState), {X2,Y2}, BoardStateTmp2)}
+  end.
+ 
+decrementPiece(Color,{WhiteCount,BlackCount}) ->
+	case {WhiteCount,BlackCount} of
+		{{Color,Count},_} -> {{Color,Count-1},BlackCount};
+		{_,{Color,Count}} -> {WhiteCount,{Color,Count-1}}
+	end.
+  
+moveOrCapture([X1,Y1,X2,Y2],BoardState) ->
+	case abs(X2 - X1) of
+		2 ->
+			setSquareValue(empty,{round((X1+X2)/2),round((Y1+Y2)/2)},BoardState);
+		_ ->
+			BoardState
+	end.
 
-getAvailableSquares(Piece,{X,Y}, BoardState, CaptureMoves) ->
+getAvailableSquares(Piece,{X,Y}, BoardState) ->
     CaptureMoves = getCaptureMoveSquares(Piece,{X,Y}, BoardState),
-    getNormalMoveSquares(Piece,{X,Y}, BoardState, CaptureMoves) ++ CaptureMoves.
+    getNormalMoveSquares(Piece,{X,Y}, BoardState) ++ CaptureMoves.
 	
-getNormalMoveSquares(Piece,{X,Y}, BoardState, []) ->
+getNormalMoveSquares(Piece,{X,Y}, BoardState) ->
   case Piece of
     {_,pawn} ->
       case Piece of
@@ -34,7 +52,7 @@ getNormalMoveSquares(Piece,{X,Y}, BoardState, []) ->
     {_,queen} -> getQueenNormalMoveSquares({X,Y},{1, 1},[]) ++ getQueenNormalMoveSquares({X,Y},{1, -1},[]) ++ getQueenNormalMoveSquares({X,Y},{-1, 1},[]) ++
       getQueenNormalMoveSquares({X,Y},{-1, -1},[])
   end;
-getNormalMoveSquares(_, _, _, _) -> [].
+getNormalMoveSquares(_, _, _) -> [].
 
 getQueenNormalMoveSquares({X,Y},{DirectionX, DirectionY},SquaresList) when (X+DirectionX >= 1) and (Y+DirectionY >= 1) and
   (X+DirectionX =< 8) and (Y+DirectionY =< 8) ->
@@ -43,7 +61,39 @@ getQueenNormalMoveSquares(_,_,SquaresList) -> SquaresList.
 
 getCaptureMoveSquares(Piece,{X,Y}, BoardState) ->
   case Piece of
-    {_,pawn} -> [];
+    {_,pawn} -> 
+      case Piece of
+        {white,_} ->
+			if X+2 < 9, Y-2 > 0 -> 
+				case getSquareValue({X+1,Y-1},BoardState) of
+					{black,_} -> R = [{X+2,Y-2}];
+					_ -> R = []
+				end;
+				true 			-> R = []
+			end,
+			if X-2 > 0, Y-2 > 0 -> 
+				case getSquareValue({X-1,Y-1},BoardState) of
+					{black,_} -> R ++ [{X-2,Y-2}];
+					_ -> R
+				end;
+			   true				-> R
+			end;
+        {black,_} ->
+			if X+2 < 9, Y+2 > 0 -> 
+				case getSquareValue({X+1,Y+1},BoardState) of
+					{white,_} -> R = [{X+2,Y+2}];
+					_ -> R = []
+				end;
+				true 			-> R = []
+			end,
+			if X-2 > 0, Y+2 > 0 -> 
+				case getSquareValue({X-1,Y+1},BoardState) of
+					{white,_} -> R ++ [{X-2,Y+2}];
+					_ -> R
+				end;
+			   true				-> R
+			end
+      end;
     {_,queen} -> []
 	% to_do
   end.
